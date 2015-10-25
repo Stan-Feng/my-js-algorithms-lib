2d3cb852d5d6825e30e38f04e2144d05e1bbc207
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2015-10-25 13:30:54 +0800

    add append() method

--- DataStructure/LinkedList.js
+++ DataStructure/LinkedList.js
@@ -4,26 +4,90 @@
  * @private: {class} Node
  * @private: {number} length
  * @private: {object} head
- * @method: append -->
- * @method: insert -->
- * @method: removeAt -->
- * @method: remove -->
- * @method: indexOf -->
- * @method: isEmpty -->
- * @method: size -->
- * @method: toString -->
- * @method: print  -->
+ * @method: append (element) --> append a new node in the end of list
+ * @method: insert (position, element) --> Insert a new element at the specified position
+ * @method: removeAt (position) --> Removes an item from specified position
+ * @method: remove (element) -->  Removes an item from list
+ * @method: indexOf (element) --> Return the index of input element
+ * @method: isEmpty --> Return whether list is empty
+ * @method: size --> Return the size of list
+ * @method: toString --> Return all element values
  *
  * @createdAt: Thu Oct 15 2015 15:24:53 GMT+0800 (CST)
  * @finiedAt:
  */
 
- function LinkedList(){
-   var _Node =  function(){
+function LinkedList() {
+	class _Node {
+		constructor(element) {
+			this.element = element;
+			this.next = null;
+		}
+	}
 
-   };
 
 
- }
+	let _length = 0;
+	let _head = null;
 
- module.exports = LinkedList;
+	this.append = (element) => {
+    let node = new _Node(element);
+    let current;
+
+		if (_head === null){
+      _head = node;
+    }
+		else {
+      current = _head;
+      while(current.next)
+        current = current.next;
+      current.next = node;
+    }
+
+    _length++;
+	};
+
+  this.removeAt = (position) => {
+    if( position < 1 || position > _length) {
+      return new Error('Invalid Input..');
+    }
+
+    let current = head;
+    let previous = head;
+    let index = 0;
+
+    if (position === 0){
+      let returnNode = Object.create(head);
+      head = current.next;
+      current = current.next;
+      return returnNode;
+    }
+
+    while(index <= position){
+      index++;
+      previous = current;
+      current = current.next;
+    }
+
+
+
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+  //Only used in test
+  this.testMethod = () => {
+    return { length : _length, head : _head};
+  };
+}
+
+module.exports = LinkedList;
--- test/LinkedList.test.js
+++ test/LinkedList.test.js
@@ -1,20 +1,20 @@
 //Date: Thu Oct 15 2015 15:24:53 GMT+0800 (CST)
 'use strict';
-var should = require('chai').should();
-var listPath = '../DataStructure/LinkedList';
+const should = require('chai').should();
+const listPath = '../DataStructure/LinkedList';
+var LinkedList = require(listPath);
 
 
-describe.only('DataStructure -- LinkedList: \n', function () {
+describe.only('DataStructure -- LinkedList: \n', () => {
 
-  describe('LinkedList -- Export: \n', function(){
-    var LinkedList = require(listPath);
+  describe('LinkedList -- Export: \n', () => {
 
-    it('should be a constructor \n ', function(){
+    it('should be a constructor \n ',  () => {
         LinkedList.should.exist;
         LinkedList.should.be.a('function');
     });
 
-    it('the constructor should create a brand new LinkedList object', function () {
+    it('the constructor should create a brand new LinkedList object', () =>{
       var l1 = new LinkedList();
       var l2 = new LinkedList();
 
@@ -25,6 +25,72 @@ describe.only('DataStructure -- LinkedList: \n', function () {
       comparasion.should.be.a('boolean');
       comparasion.should.equal(false);
     });
+
+  });
+
+  describe('LinkedList -- Methods: \n', () => {
+
+    describe('LinkedList -- should have: \n', () => {
+      var linkedlist = new LinkedList();
+
+      it('should have append() method', () => {
+        var append = linkedlist.append;
+        append.should.be.exist;
+        append.should.be.a('function');
+      });
+
+      it('should have removeAt() method', () => {
+        var removeAt = linkedlist.removeAt;
+        removeAt.should.be.exist;
+        removeAt.should.be.a('function');
+      });
+
+    });
+
+
+    describe('LinkedList -- append method', () => {
+      var linkedlist = new LinkedList();
+
+      it('should add element to the list', () => {
+        linkedlist.append(12);
+        var result = linkedlist.testMethod().length;
+        result.should.be.a('number');
+        result.should.equal(1);
+      });
+
+      it('should add the element to the end of list', () =>{
+        linkedlist.append(23);
+        var result = linkedlist.testMethod().head.next.element;
+        result.should.be.a('number');
+        result.should.not.equal(13);
+        result.should.equal(23);
+      });
+
+    });
+
+    describe('LinkedList -- removeAt method', () => {
+      var linkedlist = new LinkedList();
+      linkedlist.append(12);
+      linkedlist.append(12);
+
+      it('should check whether input is valid', () => {
+        var invalidInput1 = -1;
+        var invalidInput2 = 3;
+
+        var result1 = linkedlist.removeAt(invalidInput1);
+        var result2 = linkedlist.removeAt(invalidInput2);
+
+        result1.should.be.an('error');
+        result2.should.be.an('error');
+      });
+
+      it('should return the first element, when input position is 0', () => {
+        
+      });
+
+
+    });
+
 
   });
 
