5803f410d8e3b209329bb206d207977fd00a0c97
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2015-10-29 15:27:10 +0800

    add add method

--- DataStructure/Set.js
+++ DataStructure/Set.js
@@ -1,12 +1,53 @@
 /**
- * @class: Set
+ * @class: Set --> A collection of items that are unordered and consists of
+ *  unique elements
+ * @method: add(item) --> Add new item into set
+ * @method: remove(item) --> Remove the item of the set
+ * @method: has(item) --> Return true or false whether this item exists
+ * @method: clear() --> Clear all items
+ * @method: size() --> return the size of Set
+ * @method: values() --> return an array contains all items
  */
 
-class Set{
-	constructor() {
-    this.size = 0;
-	}
-}
+function Set(){
+  let _items = {};
+  let _size = 0;
 
+  this.add = function(item){
+    if(this.has(item))
+      return { isSuccess: false, reason: 'Element already exists.' };
+
+    _items[item] = item;
+    _size++;
+    return { isSsuccess: true };
+  };
+
+
+  this.remove = function(item){
+    if(this.has(item))
+      return { isSuccess: false, reason: 'Element does not exists.' };
+
+    _size--;
+    _items[item] = undefined;
+  };
+
+  this.has = function(item){
+    return _items.hasOwnProperty(item) || _items[item] !== undefined;
+  };
+
+  this.clear = function(){
+
+  };
+
+  this.size = function(){
+    return _size;
+  };
+
+  this.values = function(){
+
+  };
+
+
+}
 
 export default Set;
--- test/Set.spec.js
+++ test/Set.spec.js
@@ -6,11 +6,41 @@ import Set from '../DataStructure/Set';
 import commonTest from './common.spec';
 
 describe.only('DataStructure -- Set: \n', () => {
-  commonTest(Set, ['size']);
+  commonTest(Set, ['add', 'remove', 'has', 'clear', 'size', 'values']);
 
-  it('should...', () => {
-
+  let set;
+  beforeEach(() => {
+    set = new Set();
   });
+
+  describe('Set -- has(item) method: \n ', () => {
+    it('should return false when set does not contain element \n', () => {
+      let result = set.has(2);
+      result.should.equal(false);
+    });
+  });
+
+  describe('Set -- add(item) method: \n ', () => {
+    it('should return error when added element alread exists \n', () => {
+      set.add(1);
+      let result = set.add(1);
+      result.should.be.an('object');
+      result.should.have.property('isSuccess');
+      result.should.have.property('reason');
+      result.isSuccess.should.be.false;
+      result.reason.should.exist;
+    });
+
+    it('should item add correctly \n', () => {
+      set.add(1);
+
+      let length = set.size();
+      length.should.equal(1);
+    });
+  });
+
+
+
 });
 
 
