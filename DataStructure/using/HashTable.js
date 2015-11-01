09446ea3ee2de57fb6b7a2f192d23fa29c49b500
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2015-11-01 13:35:19 +0800

    add collision solution for HashTable

--- DataStructure/using/HashTable.js
+++ DataStructure/using/HashTable.js
@@ -1,4 +1,5 @@
 'use strict';
+let LinkedList = require('../LinkedList');
 
 function HashTable(hashFunc) {
   var _table = [];
@@ -7,19 +8,78 @@ function HashTable(hashFunc) {
 
 
   this.put = (key, value) => {
-    table[_hashFunc(key)] = value;
+    var position = _hashFunc(key);
+    if(table[position] === undefined){
+      table[position] = new LinkedList;
+    }
+
+    table[position].append(new ValuePair(key, value));
     _size++;
   };
 
   this.remove = (key) => {
+    var position = _hashFunc(key);
+
+    if(table[position] !== undefined){
+      var currentNode = table[position].getHead();
+
+      while (currentNode.next){
+        if(currentNode.element.key === key){
+          table[position].remove(currentNode.element);
+        }
+
+        if(table[position].isEmpty()){
+          table[position] = undefined;
+        }
+
+        currentNode = currentNode.next;
+      }
+
+      //Check in case of first of last element
+      if(currentNode.element.key === key) {
+        table[position].remove(currentNode.element);
+
+        if(table[position].isEmpty()){
+          table[position] = undefined;
+        }
+
+        return true;
+      }
+
+      return false;
+    }
+
     delete table[_hashFunc(key)];
     _size--;
   };
 
   this.get = (key) => {
-    return table[_hashFunc(key)];
+    var position = _hashFunc(key);
+
+    if(table[position] !== undefined){
+        var currentNode = table[position].getHead();
+
+        while (currentNode.next) {
+          if (currentNode.element.key === key){
+            return currentNode.element.value;
+          }
+          currentNode = currentNode.next;
+        }
+
+        //Check start position or last position
+        if(currentNode.element.key === key){
+          return currentNode.element.value;
+        }
+    }
+
+    return undefined;
   };
 
+
+  function ValuePair(key, value) {
+    this.key = key;
+    this.value = value;
+  }
 
   function loseloseHashCode(key) {
     var hash = 0;
