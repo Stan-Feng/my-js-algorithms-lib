8fe5a2f54a2569455fbd8996e5db698bc141e585
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2015-11-06 16:15:53 +0800

    insert method self-recursive pattern

--- DataStructure/AVLTree.js
+++ DataStructure/AVLTree.js
@@ -1,12 +1,59 @@
 'use strict';
 
-
 function AVLTree() {
+  var __root = null;
+  var __Node = function(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  };
+
+  this.insert = function insertHelper(key, currentNode) {
+    if(!__root){
+      __root = new __Node(key);
+      return true;
+    }
+    
+    currentNode = currentNode || __root;
+
+    if(currentNode.key > key){
+      if(currentNode.left === null){
+        currentNode.left = new __Node(key);
+        return true;
+      }
+      //Go left
+      insertHelper(key, currentNode.left);
+    }
+    else {
+      if(currentNode.right === null){
+        console.log(currentNode);
+        currentNode.right = new __Node(key);
+        return true;
+      }
+      //Go right
+      insertHelper(key, currentNode.right);
+    }
+
+    return false;
+  };
+
+  this.search = function(key) {
+
+  };
+
+  this.delete = function(key) {
+
+  };
+
+  this.traversal = function() {
+
+  };
+
+  this.getRoot = function() {
+    return __root;
+  };
 
 }
-
-
-
 
 
 module.exports = AVLTree;
--- test/AVLTree.spec.js
+++ test/AVLTree.spec.js
@@ -5,12 +5,39 @@ const commonTest = require('./common.spec');
 const AVLTree= require('../DataStructure/AVLTree');
 
 describe.only('DataStructure -- AVL Tree: \n', () => {
-  commonTest(AVLTree, []);
+  commonTest(AVLTree, ['search', 'traversal', 'insert', 'delete']);
 
   var tree;
   beforeEach(() => {
     tree = new AVLTree();
   });
 
-  
+  describe('AVL Tree -- insert(key) method \n', () => {
+    it('should insert a key to root node when tree is empty \n', () => {
+      tree.insert(2);
+
+      var result = tree.getRoot();
+      result.should.be.an('object');
+      result.should.have.property('key');
+      result.should.have.property('left');
+      result.should.have.property('right');
+      result.key.should.equal(2);
+    });
+
+    it('should insert small key on the left, large key on the right \n', () =>{
+      tree.insert(2);
+      tree.insert(1);
+      tree.insert(3);
+
+      var root = tree.getRoot();
+      root.left.key.should.equal(1);
+      root.right.key.should.equal(3);
+    });
+
+    it('should insert node recursively \n', () => {
+
+    });
+
+  });
+
 });
