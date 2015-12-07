41eb6bd3c9d5905962f3afabad2472bab1f022e5
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2015-12-07 19:41:29 +0800

    Mon Dec  7 19:41:29 CST 2015

--- Algorithms/Euclid's.js
+++ Algorithms/Euclid's.js
@@ -2,11 +2,21 @@
 
 export default gcd;
 
-function gcd (largeInt, smallInt)  {
+function gcd (largeInt, smallInt) {
   if(largeInt < smallInt) {
     return new Error('Invalid Input Parameters.');
   }
+  else if(largeInt === undefined || smallInt === undefined) {
+    return new Error('Not enough parameters passed in.');
+  }
+  else if(smallInt === 0) {
+    return largeInt;
+  }
 
+  let modResult = largeInt % smallInt;
+  if(modResult === 0) {
+    return smallInt;
+  }
 
-
+  return gcd(smallInt, modResult);
 }
--- test/Euclid's.spec.js
+++ test/Euclid's.spec.js
@@ -1,8 +1,39 @@
-const should = require('chai').should();
 const expect = require('chai').expect;
+const should = require('chai').should();
 
-const gcd = require('../Algorithms/Euclid\'s.js');
+import gcd from '../Algorithms/Euclid\'s.js';
 
-describe.only('Euclid\'s Algorithm -- Find the largest division: \n', () => {
+describe('Euclid\'s Algorithm -- Find the largest division: \n', () => {
 
+  describe('Parameters: \n', () => {
+    it('It should import successfully. \n', () => {
+      gcd.should.be.a('function');
+    });
+
+    it('It should accept two parameters. \n', () => {
+      gcd.length.should.equal(2);
+    });
+
+    it('It should throw error when input order is not correct. \n', () => {
+      let result = gcd(0, 1);
+      result.should.be.an('error');
+    });
+
+    it('It should throw error when the input parameters are not two. \n', () => {
+      let result = gcd(1);
+      result.should.be.an('error');
+    });
+
+    it('It should return the int when input is (int, 0)', () => {
+      gcd(1, 0).should.equal(1);
+    });
+
+  });
+
+  describe('Result: \n', () => {
+    it('It should return 12 when input is (60, 24)', () => {
+      gcd(60, 24).should.equal(12);
+    });
+
+  });
 });
