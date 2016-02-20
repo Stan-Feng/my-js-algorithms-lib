c53ddcd62dc8d81885bdf1227455f700b8a15def
fengjunwenstan <fengjunwenxjtlu@gmail.com>
2016-02-20 22:47:55 +0800

    Add is Directede Acyclic Graph Method

--- DataStructure/DirectedGraph.js
+++ DataStructure/DirectedGraph.js
@@ -1,30 +1,115 @@
-export const DirectedGraph = {
-  adj: {},
-  vertices: [],
-
+const DirectedGraph = {
   init (nodes) {
-    var g = Object.create(this);
+    const g = Object.create(this);
 
+    // Attributes
+    this.adjList = {};
+    this.vertices = [];
+
+    // Initialization
     nodes.forEach(node => {
-      var { to, source } = node;
+      const { to, label } = node;
 
-      g.adj[source] = to;
-      g.vertices.push(source);
+      g.adjList[label] = { to };
+      g.vertices.push(label);
     });
+    g.root = g.vertices[0];
 
     return g;
   },
 
-  // Accept two source label as parameter, add edge from v to w
+  deleteNode (label) {
+    const index = this.vertices.indexOf(label);
+    this.vertices.splice(index, 1);
+
+    delete this.adjList[label];
+  },
+
+  // Add edge from 'v' to 'w'
   addEdge (v, w) {
-    adj[v].push(w);
+    adjList[v].push(w);
   },
 
-  getAdjList () {
-    return this.adj;
+  // If tarLabel === startLabel return false --> this graph is not Directed Acyclic Graph
+  isDag (startLabel, tarLabel) {
+    if (!startLabel && !tarLabel) {
+      startLabel = this.root;
+    }
+    // console.log(`StartLabel ${startLabel} : tarLabel ${tarLabel}`);
+    if (!tarLabel) {
+      const dest = this.adjList[startLabel].to;
+
+      for (let i = 0; i < dest.length; i++) {
+        if (!this.isDag(dest[i], startLabel)) {
+          return false;
+        }
+      }
+
+      return true;
+    } else {
+
+      if (startLabel === tarLabel) {
+        return false;
+      } else {
+        const dest = this.adjList[startLabel].to;
+
+        for (let i = 0; i < dest.length; i++) {
+          if (!this.isDag(dest[i], tarLabel)) {
+            return false;
+          }
+        }
+
+        return true;
+      }
+    }
   },
 
-  getVerts () {
-    return this.vertices;
+  // Decrease and conquer
+  topologicalSort () {
+    if (!this.isDag(this.root)) {
+      return new Error('Directed Acyclic Graph cannot use topological sort algorithm.');
+    }
+    const orderedList = [];
+    const remainGraph = Object.create(this);
+    const remainAdjList = remainGraph.adjList;
+    const remainVertices = remainGraph.vertices;
+
+    // Check whether exists incoming edge from root node
+    for (let i = 0; i < remainVertices.length; i++) {
+      console.log(remainVertices);
+      const label = remainVertices[i];
+      // Find whether remaining node has edge to 'node'
+      for (let j = i + 1; j < remainVertices.length; j++) {
+        const remainLabel = remainVertices[j];
+        if (remainAdjList[remainLabel].to.indexOf(label) >= 0) {
+          // There exists edge coming in
+          break;
+        }
+      }
+      // No incoming edge, delete this node
+      console.log(`${label} has been removed from list...`);
+      i -= 1;
+      orderedList.push(label);
+      remainGraph.deleteNode(label);
+    }
+
+    return orderedList;
+
+    // var list = {};
+    //
+    // for (let i = 0; i < vertices.length; i++) {
+    //   // Vertice Label
+    //   let label = vertices[i];
+    //   // Start Node
+    //   let node = adjList[label];
+    //   // For each node in adjList
+    //   for (let symbole in adjList) {
+    //     if (adjList.hasOwnProperty(symbole)) {
+    //       // console.log(symbole);
+    //     }
+    //   }
+    // }
   }
 };
+
+export { DirectedGraph };
--- test/DirectedGraph.spec.js
+++ test/DirectedGraph.spec.js
@@ -1,26 +1,26 @@
 import chai from 'chai';
 import { DirectedGraph } from '../DataStructure/DirectedGraph';
 
+const expect = chai.expect;
 const should = chai.should();
 
 describe.only('DataStructure -- Directed Graph \n', () => {
-  describe('Init functionalities \n', () => {
+  describe('Init functionalities && Simple Case \n', () => {
     var graph;
-    var nodes = [
-      { source: 'a', to: ['b', 'c'] },
-      { source: 'b', to: ['d'] },
-      { source: 'c', to: ['b'] },
-      { source: 'd', to: ['a'] }
+    const nodes = [
+      { label: 'a', to: ['b', 'c'] },
+      { label: 'b', to: ['d'] },
+      { label: 'c', to: ['b'] },
+      { label: 'd', to: ['a'] }
     ];
+
     beforeEach(() => {
-      graph = undefined;
+      graph = DirectedGraph.init(nodes);
     });
 
     it('It should add all nodes to adj list correctly. \n', () => {
-      graph = DirectedGraph.init(nodes);
-
-      var vertices = graph.getVerts();
-      var adjList = graph.getAdjList();
+      var vertices = graph.vertices;
+      var adjList = graph.adjList;
 
       vertices.should.have.length(4);
       adjList.should.have.property('a');
@@ -28,10 +28,44 @@ describe.only('DataStructure -- Directed Graph \n', () => {
       adjList.should.have.property('c');
       adjList.should.have.property('d');
 
-      adjList.a.should.have.length(2);
-      adjList.b.should.have.length(1);
-      adjList.c.should.have.length(1);
-      adjList.d.should.have.length(1);
     });
+
+    it('It should delete node correctly. \n', () => {
+      graph = DirectedGraph.init(nodes);
+      graph.deleteNode('a');
+
+      graph.vertices.indexOf('a').should.equal(-1);
+      expect(graph.adjList['a']).to.be.undefined;
+    });
+
+    it('It should not be a dag DirectedGraph. \n', () => {
+      graph.isDag('a').should.be.false;
+    });
+  });
+
+  describe('It should be executed in a more complex example. \n', () => {
+    var graph;
+    var nodes = [
+      { label: 'a', to: ['b', 'c'] }, // 0
+      { label: 'b', to: ['c', 'd'] },  // 1
+      { label: 'c', to: ['f'] },  // 2
+      { label: 'e', to: ['f'] }, // 3
+      { label: 'd', to: ['g', 'h'] }, // 4
+      { label: 'f', to: ['g', 'h'] }, // 5
+      { label: 'g', to: [] }, // 6
+      { label: 'h', to: [] } // 7
+    ];
+
+    it('It should be a directed acyclic graph. \n', () => {
+      graph = DirectedGraph.init(nodes);
+      graph.isDag().should.be.true;
+    });
+
+    it('It should return a topological sorted list. \n', () => {
+      graph = DirectedGraph.init(nodes);
+      var list = graph.topologicalSort();
+      console.log(list);
+    });
+
   });
 });
